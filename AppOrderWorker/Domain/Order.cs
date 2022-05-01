6a275e9a7b2e8140a3c21e8829d726e74e48c8a4
Douglas Emerick <douglas@emerick.net.br>
2022-05-01 17:57:31 -0300

    Implementado o Worker para consumir a fila

---
+++ AppOrderWorker/Domain/Order.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppOrderWorker.Domain;
+public class Order
+{
+    public int OrderNumber { get; set; }
+public string? ItemName { get; set; }
+public float Price { get; set; }
+}
---
+++ AppOrderWorker/Program.cs
@@ -0,0 +1,51 @@
+﻿using AppOrderWorker.Domain;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System.Text;
+
+public class Program
+{
+    public static void Main()
+    {
+
+        var factory = new ConnectionFactory() { HostName = "localhost" };
+        using (var connection = factory.CreateConnection())
+        using (var channel = connection.CreateModel())
+        {
+            channel.QueueDeclare(queue: "orderQueue",
+                                 durable: false,
+                                 exclusive: false,
+                                 autoDelete: false,
+                                 arguments: null);
+
+            var consumer = new EventingBasicConsumer(channel);
+            consumer.Received += (model, ea) =>
+            {
+                try
+                {
+
+
+                    var body = ea.Body.ToArray();
+                    var message = Encoding.UTF8.GetString(body);
+                    var order = System.Text.Json.JsonSerializer.Deserialize<Order>(message);
+
+                    Console.WriteLine($"Order: {order!.OrderNumber} | {order.ItemName} | {order.Price}");
+
+                    channel.BasicAck(ea.DeliveryTag, false);
+                }
+                catch (Exception ex)
+                {
+                    //Logger
+                    channel.BasicNack(ea.DeliveryTag, false, true);
+                }
+            };
+            channel.BasicConsume(queue: "orderQueue",
+                                 autoAck: false,
+                                 consumer: consumer);
+
+            Console.WriteLine(" Press [enter] to exit.");
+            Console.ReadLine();
+        }
+
+    }
+}
